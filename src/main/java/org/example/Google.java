3e4c160bdcd7673d63b613fbe10ed4b755878aec
AakashNandi <aakashnandi3@gmail.com~>
2026-02-03 09:05:45 +0000

    Push

--- src/main/java/org/example/Google.java
+++ src/main/java/org/example/Google.java
@@ -1,5 +1,27 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Objects;
+
 public class Google {
-    public void static main(String args[])
+    public static void main(String[] args) throws Exception{
+        WebDriver driver = new ChromeDriver();
+        driver .get("https://www.google.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement ele = driver.findElement(By.xpath("//textarea[@name='q']"));
+        Objects.requireNonNull(wait.until(ExpectedConditions.visibilityOf(ele))).sendKeys("frescoapps", Keys.ENTER);;
+        Thread.sleep(30000);
+        WebElement ele2 = driver.findElement(By.xpath("//h3"));
+        Objects.requireNonNull(wait.until(ExpectedConditions.elementToBeClickable(ele2))).click();
+        //ele2.click();
+
+    }
 }
